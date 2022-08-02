609d9d14fb4b74a050f11266bb2f6c4adbc3add6
Gabriel <gabrieljch929@gmail.com>
2022-08-02 14:43:31 -0500

    work on activities

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,41 +1,71 @@
 import React, { useState,useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { getActivity } from "../api";
+import { getActivities } from "../api";
 
 
 
-const allActivities = () => {
-  const token = localStorage.getItem("token")
-  useEffect(() => {
-    getActivity().then((result) => {
-      setActivities(result.activities);
-    });
-  }, []);
+// const allActivities = () => {
+//   const token = localStorage.getItem("token")
+//   useEffect(() => {
+//     getActivities().then((result) => {
+//       setActivities(result.activities);
+//     });
+//   }, []);
   
-  // console.log(token);
+//   // console.log(token);
 
 
-  return (
+//   return (
+//     <>
+//       <h1>Activities</h1>
+//       {activities.map((activity) => {
+//         let activityId = activity._id;
+//         return(
+//            <div key={activityId} className="Activities">
+//           <h2>{activity.name}</h2>
+//           <p>{activity.description}</p>
+          
+          
+          
+//         </div>
+//         )
+       
+//         })}
+//     </>
+//   );
+// };
+const [activitiesArray, setActivitiesArray]=useState([])
+const [activityName, setActivityName]=useState('')
+const [activitiesDes, setActivitiesDes]=useState('')
+export async function allActivities (){
+try {
+  const result= await getActivities()
+  setActivitiesArray(result)
+} catch (error) {
+  console.error(error)
+}
+useEffect (()=>{
+  allActivities()
+},[])
+return(
     <>
       <h1>Activities</h1>
       {activities.map((activity) => {
         let activityId = activity._id;
-        return(
+       
            <div key={activityId} className="Activities">
           <h2>{activity.name}</h2>
           <p>{activity.description}</p>
           
           
           
-        </div>
-        )
+        </div> 
+})
        
-        })}
-    </>
-  );
-};
+        }
+    </> )
 
 
 
 
-export default allActivities
+export default Activities
