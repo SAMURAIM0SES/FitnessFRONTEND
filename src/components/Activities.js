824a5f45491a6d166460c6b70166539727ae0aad
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-08-03 12:18:17 -0500

    back from lunch

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -4,76 +4,88 @@ import { getActivities } from "../api";
 
 
 
-// const allActivities = () => {
-//   const token = localStorage.getItem("token")
-//   useEffect(() => {
-//     getActivities().then((result) => {
-//       setActivities(result.activities);
-//     });
-//   }, []);
-  
-//   // console.log(token);
-
-
-//   return (
-//     <>
-//       <h1>Activities</h1>
-//       {activities.map((activity) => {
-//         let activityId = activity._id;
-//         return(
-//            <div key={activityId} className="Activities">
-//           <h2>{activity.name}</h2>
-//           <p>{activity.description}</p>
-          
-          
-          
-//         </div>
-//         )
-       
-//         })}
-//     </>
-//   );
-// };
-// const [activityName, setActivityName]=useState('')
-// const [activitiesDes, setActivitiesDes]=useState('')
-function AllActivities (){
-  const [activities, activitiesArray, setActivitiesArray]=useState([])
-  try {
-  const result= getActivities()
-  setActivitiesArray(result)
-} catch (error) {
-  console.error(error)
-}
-useEffect (()=>{
-  AllActivities()
-},[])
-return (
+const AllActivities = () => {
+ try{
+    const {activities, setActivities} = useState([])
+    const token = localStorage.getItem("token")
+    const result = getActivities()
+        setActivities(result);
     
-  <div className="activities">
+  }catch(error){
+    console.log(error, "woooooah something broke on line 15")
+  }
+  
+  useEffect(() => {
+    getActivities()
+}, [])
+  
+  // console.log(token);
 
-  (<>
-    {/* {activities.map((activities) => { */}
-      return (
-        <div
-          className="activities"
-          key={activities._id}
-          style={{
-            alignItems: "center",
-            margin: "20px 60px",
-            border: "1px solid black",
-            padding: "10px",}}>
-          <h3>{activities.name}</h3>
-          <h4>{activities.id}</h4>
-          <p>{activities.description}</p>
+
+  return (
+    <>
+      <h1>Activities</h1>
+      
+        
+        
+           <div key={activity.id} className="Activities">
+          <h2>{result.activity.name}</h2>
+          <p>{activity.description}</p>
+          
+          
           
         </div>
-      );
-    )
-    </>
+        
     
-  </div>
-);
+        
+    </>
+  );
 };
+
+
+
+
+
+
+
+// function AllActivities (){
+//   const [activities, activitiesArray, setActivitiesArray]=useState([])
+//   try {
+//   const result= getActivities()
+//   setActivitiesArray(result)
+// } catch (error) {
+//   console.error(error)
+// }
+// useEffect (()=>{
+//   AllActivities()
+// },[])
+// return (
+    
+//   <div className="activities">
+
+//   (<>
+//     {activities.map((activities) => {
+//       return (
+//         <div
+//           className="activities"
+//           key={activities._id}
+//           style={{
+//             alignItems: "center",
+//             margin: "20px 60px",
+//             border: "1px solid black",
+//             padding: "10px",}}>
+//           <h3>{activities.name}</h3>
+//           <h4>{activities.id}</h4>
+//           <p>{activities.description}</p>
+          
+//         </div>
+//       );
+//     )
+//     </>
+    
+//   </div>
+// );
+// };
 
 
 
