6c1178bb18470b77b27c7477b53c55867bf99569
Gabriel <gabrieljch929@gmail.com>
2022-08-04 10:33:16 -0500

    Fixing branch

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { getActivity } from "../api";
+import { getActivities } from "../api";
 
 
 
